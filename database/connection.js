4396ec854004cef11e5b9a9a2c30afe7f18281ed
artursousac <artursousac@hotmail.com>
2024-05-11 21:30:15 -0300

    configurando respositorio

---
+++ database/connection.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose")
+
+const dbUser = process.env.DB_USER
+const dbPassword = process.env.DB_PASSWORD
+
+const connect = () => {
+    mongoose.connect(`mongodb+srv://artursousac:ZWw9iafqnMBeHOG4@cluster0.brp8cuh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
+
+    const connection = mongoose.connection;
+
+    connection.on("error", () => {
+        console.error("Erro ao conectar com o mongoDB")
+    })
+
+    connection.on("open", () => {
+        console.log("Conectado ao mongoDB com sucesso")
+    })
+}
+
+connect();
+
+module.exports = mongoose;
+

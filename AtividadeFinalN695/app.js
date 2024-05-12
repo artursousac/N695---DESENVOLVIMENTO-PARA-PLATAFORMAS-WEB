7a2b15dab444d4bd8ac4f23e74583fe3cd1cc4be
artursousac <artursousac@hotmail.com>
2024-05-11 21:00:03 -0300

    configurando conexao com database e demais pacotes

--- AtividadeFinalN695/app.js
+++ AtividadeFinalN695/app.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const cors = require("cors")
 
+require("dotenv").config()
+
 var app = express();
 
 app.use(cors())
@@ -10,3 +12,5 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Servidor rodando na porta: ${port}`)
 })
+
+require("./database/connection")

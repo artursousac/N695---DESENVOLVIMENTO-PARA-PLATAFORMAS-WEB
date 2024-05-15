7355a6fc7059f6b5e9ea1861bac45a85c774e0a8
artursousac <artursousac@hotmail.com>
2024-05-14 21:34:44 -0300

    tela de login com validacoes realizada e navegacao para tela inicial da aplicacao

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 import express from 'express'
 import connectDatabase from './database/db.js'
 import routes from './routes.js'
+import cors from 'cors'
 
 const app = express();
 
+app.use(cors())
 app.use(express.json());
 app.use(routes)
+
 
 connectDatabase().then(
     () => {app.listen(3000, () => console.log("Servidor rodando e banco de dados conectado"))})
